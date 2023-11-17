9d2c8ab3048e53fc1cb8274ee292776fd67b35b3
Tomer Atar <kenyweb7@gmail.com>
2023-11-17 17:40:30 +0200

    fix: generated/ in gitignore + fix miners fruit texture

--- src/main/java/com/bajookie/biotech/item/ModItems.java
+++ src/main/java/com/bajookie/biotech/item/ModItems.java
@@ -14,23 +14,25 @@ import static com.bajookie.biotech.BioTech.MOD_ID;
 @SuppressWarnings("SpellCheckingInspection")
 public class ModItems {
     //items list
-    public static final Item FLUID_CELL = registerItem("fluid_cell",new Item(new FabricItemSettings()));
-    public static final Item SULFUR = registerItem("sulfur",new Item(new FabricItemSettings()));
-    public static final Item ORANGE = registerItem("orange",new Item(new FabricItemSettings().food(ModFoodComponents.ORANGE)));
-    public static final Item LEMON = registerItem("lemon",new Item(new FabricItemSettings().food(ModFoodComponents.LEMON)));
+//    public static final Item FLUID_CELL = registerItem("fluid_cell",new Item(new FabricItemSettings()));
+//    public static final Item SULFUR = registerItem("sulfur",new Item(new FabricItemSettings()));
+//    public static final Item ORANGE = registerItem("orange",new Item(new FabricItemSettings().food(ModFoodComponents.ORANGE)));
+//    public static final Item LEMON = registerItem("lemon",new Item(new FabricItemSettings().food(ModFoodComponents.LEMON)));
+//    public static final Item CITRUSJUICE = registerItem("bottle_of_citrus_juice",new Item(new FabricItemSettings().food(ModFoodComponents.CITRUS_JUICE)));
+//    public static final Item UNFERMENTEDBIOMASS = registerItem("unfermented_biomass",new Item(new FabricItemSettings()));
+//    public static final Item FERMENTEDBIOMASS = registerItem("fermented_biomass",new Item(new FabricItemSettings()));
+//    public static final Item MORTAR_AND_PESTLE = registerItem("mortar_and_pestle",new MortarAndPestleItem(new FabricItemSettings().maxDamage(100)));
+    // legacy ^
     public static final Item EXPLORER_FRUIT = registerItem("explorers_fruit",new Item(new FabricItemSettings().food(ModFoodComponents.EXPLORERS_FRUIT).maxCount(16)));
     public static final Item MINERS_FRUIT = registerItem("miners_fruit",new Item(new FabricItemSettings().food(ModFoodComponents.MINERS_FRUIT).maxCount(16)));
-    public static final Item CITRUSJUICE = registerItem("bottle_of_citrus_juice",new Item(new FabricItemSettings().food(ModFoodComponents.CITRUS_JUICE)));
-    public static final Item UNFERMENTEDBIOMASS = registerItem("unfermented_biomass",new Item(new FabricItemSettings()));
-    public static final Item FERMENTEDBIOMASS = registerItem("fermented_biomass",new Item(new FabricItemSettings()));
-    public static final Item MORTAR_AND_PESTLE = registerItem("mortar_and_pestle",new MortarAndPestleItem(new FabricItemSettings().maxDamage(100)));
 
     //Register methods
     public static Item registerItem(String name,Item item){
         return Registry.register(Registries.ITEM,new Identifier(MOD_ID,name),item);
     }
     public static void registerModItems(){
+
+        // FuelRegistry.INSTANCE.add(FERMENTEDBIOMASS,6400);
         BioTech.LOGGER.info("Register Items for:"+MOD_ID);
-        FuelRegistry.INSTANCE.add(FERMENTEDBIOMASS,6400);
     }
 }
