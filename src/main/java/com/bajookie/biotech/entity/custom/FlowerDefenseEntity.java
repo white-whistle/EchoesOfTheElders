6b25600d9a505708975c3d20b928efa7a460c68a
liorb <lior.buchmeier@gmail.com>
2023-11-29 20:28:22 +0200

    close part time

--- src/main/java/com/bajookie/biotech/entity/custom/FlowerDefenseEntity.java
+++ src/main/java/com/bajookie/biotech/entity/custom/FlowerDefenseEntity.java
@@ -1,15 +1,20 @@
 package com.bajookie.biotech.entity.custom;
 
+import com.bajookie.biotech.BioTech;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.ActiveTargetGoal;
 import net.minecraft.entity.ai.goal.LookAroundGoal;
 import net.minecraft.entity.ai.goal.LookAtEntityGoal;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
@@ -22,13 +27,24 @@ public class FlowerDefenseEntity extends AnimalEntity {
     @Override
     protected void initGoals() {
         this.goalSelector.add(0, new LookAtEntityGoal(this, PlayerEntity.class,5f));
-        this.goalSelector.add(0, new LookAroundGoal(this));
+        this.goalSelector.add(1, new LookAroundGoal(this));
     }
 
     public static DefaultAttributeContainer.Builder createFlowerDefenseAttributes(){
         return MobEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH,15)
                 .add(EntityAttributes.GENERIC_ARMOR,5f);
+    }
+
+    @Override
+    public ItemStack eatFood(World world, ItemStack stack) {
+        return Items.APPLE.getDefaultStack();
+    }
+
+    @Override
+    public void onDeath(DamageSource damageSource) {
+        BioTech.LOGGER.info("dead");
+        //ModRaid modRaid = new ModRaid(2,this,getWorld());
     }
 
     @Nullable
