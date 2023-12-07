4ebc9ab57214a976df2b1a0ecaf0029375de4fbe
liorb <lior.buchmeier@gmail.com>
2023-12-07 21:17:53 +0200

    added the Gale Core V2

--- src/main/java/com/bajookie/biotech/item/custom/GaleCoreItem.java
+++ src/main/java/com/bajookie/biotech/item/custom/GaleCoreItem.java
@@ -8,11 +8,14 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.FireworkRocketEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.item.Vanishable;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 import static com.bajookie.biotech.BioTech.MOD_ID;
@@ -30,23 +33,30 @@ public class GaleCoreItem extends Item {
         if (stack.hasNbt()){
             if (entity instanceof PlayerEntity user) {
 
-                BioTech.LOGGER.info(String.valueOf(stack.getDamage()));
                 if (!stack.getOrCreateNbt().contains(use_id)) {
                     stack.getOrCreateNbt().putInt(use_id, 0);
                     stack.getOrCreateNbt().putBoolean(active_id, false);
                 }
                 int usage = stack.getNbt().getInt(use_id);
                 if (stack.getNbt().getBoolean(active_id)) {
+
+                    Vec3d vec3d3 = Vec3d.ZERO;
+                    if (user.isFallFlying()) {
+                        world.addParticle(ParticleTypes.POOF,user.getX(),user.getY(),user.getZ(),0,0,0);
+                        Vec3d vec3d = user.getRotationVector();
+                        double d = 1.5;
+                        double e = 0.1;
+                        Vec3d vec3d2 = user.getVelocity();
+                        user.setVelocity(vec3d2.add(vec3d.x * 0.1 + (vec3d.x * 1.5 - vec3d2.x) * 0.5, vec3d.y * 0.1 + (vec3d.y * 1.5 - vec3d2.y) * 0.5, vec3d.z * 0.1 + (vec3d.z * 1.5 - vec3d2.z) * 0.5));
+                        vec3d3 = user.getHandPosOffset(Items.FIREWORK_ROCKET);
+                    } else {
+                        world.addParticle(ParticleTypes.POOF,user.getX(),user.getY(),user.getZ(),0,-2,0);
+                        vec3d3 = Vec3d.ZERO;
+
+                    }
                     if (usage > 10) {
                         stack.getNbt().putInt(use_id, 0);
                         stack.setDamage(stack.getDamage() + 2);
-                        if (user.isFallFlying()) {
-                            ItemStack itemStack = user.getStackInHand(user.getActiveHand());
-                            if (!world.isClient) {
-                                FireworkRocketEntity fireworkRocketEntity = new FireworkRocketEntity(world, itemStack, user);
-                                world.spawnEntity(fireworkRocketEntity);
-                            }
-                        }
 
                     } else {
                         stack.getNbt().putInt(use_id, usage + 1);
@@ -85,7 +95,6 @@ public class GaleCoreItem extends Item {
 
     @Override
     public boolean hasGlint(ItemStack stack) {
-        BioTech.LOGGER.info("call");
         if (stack.hasNbt()){
             if (stack.getNbt().contains(active_id)){
                 return stack.getNbt().getBoolean(active_id);
