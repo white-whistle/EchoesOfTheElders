c7a033609aa4d050ba10c2e35b1003dc7eb488ef
liorb <lior.buchmeier@gmail.com>
2023-12-07 18:31:28 +0200

    added the hammer V2

--- src/main/java/com/bajookie/biotech/item/custom/MidasHammerItem.java
+++ src/main/java/com/bajookie/biotech/item/custom/MidasHammerItem.java
@@ -1,9 +1,14 @@
 package com.bajookie.biotech.item.custom;
 
+import com.bajookie.biotech.BioTech;
 import net.minecraft.client.option.ParticlesMode;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageSources;
+import net.minecraft.entity.damage.DamageType;
+import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -26,10 +31,18 @@ public class MidasHammerItem extends PickaxeItem {
     @Override
     public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
         if (!user.getItemCooldownManager().isCoolingDown(this)){
-            user.getItemCooldownManager().set(this,20*60*5);
+            user.getItemCooldownManager().set(this,20*6);
             entity.getWorld().addParticle(ParticleTypes.SMOKE,entity.getX(),entity.getY(),entity.getZ(),0,1,0);
-            ItemEntity item = new ItemEntity(entity.getWorld(),entity.getX(),entity.getY(),entity.getZ(),new ItemStack(Items.GOLD_INGOT,6));
-            entity.getWorld().spawnEntity(item);
+            if(entity.getHealth()<=50){
+                int hp = Math.round(entity.getHealth());
+                int nugget = hp % 9;
+                int ingot = (hp-nugget)/9;
+                ItemEntity item = new ItemEntity(entity.getWorld(),entity.getX(),entity.getY(),entity.getZ(),new ItemStack(Items.GOLD_INGOT,ingot));
+                ItemEntity itemN = new ItemEntity(entity.getWorld(),entity.getX(),entity.getY(),entity.getZ(),new ItemStack(Items.GOLD_NUGGET,nugget));
+                entity.getWorld().spawnEntity(item);
+                entity.getWorld().spawnEntity(itemN);
+            }
+            entity.damage(entity.getWorld().getDamageSources().magic(),50f);
         }
         return super.useOnEntity(stack, user, entity, hand);
     }
