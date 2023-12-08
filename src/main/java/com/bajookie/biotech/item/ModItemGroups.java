5d6e8e59f177237d7faf7cd09e22baff8b086c30
Tomer Atar <kenyweb7@gmail.com>
2023-12-08 12:05:29 +0200

    feat: silent fire wip

--- src/main/java/com/bajookie/biotech/item/ModItemGroups.java
+++ src/main/java/com/bajookie/biotech/item/ModItemGroups.java
@@ -14,20 +14,20 @@ import static com.bajookie.biotech.BioTech.MOD_ID;
 
 @SuppressWarnings("unused")
 public class ModItemGroups {
-    public static final ItemGroup MOD_ITEM_GROUP = Registry.register(Registries.ITEM_GROUP,new Identifier(MOD_ID,"mod_item_group"),
-            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.bio")).icon(()->new ItemStack(ModBlocks.MINERS_FRUIT_BLOCK)).entries((displayContext, entries) -> {
+    public static final ItemGroup MOD_ITEM_GROUP = Registry.register(Registries.ITEM_GROUP, new Identifier(MOD_ID, "mod_item_group"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.bio")).icon(() -> new ItemStack(ModBlocks.MINERS_FRUIT_BLOCK)).entries((displayContext, entries) -> {
                 entries.add(ModItems.EXPLORER_FRUIT);
-                //blocks
+                // blocks
                 entries.add(ModBlocks.BEAR_TRAP_BLOCK);
-                //flowers
+                // flowers
                 entries.add(ModBlocks.EXPLORER_FRUIT_BLOCK);
                 entries.add(ModBlocks.MINERS_FRUIT_BLOCK);
                 entries.add(ModBlocks.NETHER_FRUIT_BLOCK);
                 entries.add(ModBlocks.RELIC_CONTAINER_BLOCK);
             }).build());
 
-    public static final ItemGroup MOD_ARTIFACT_GROUP = Registry.register(Registries.ITEM_GROUP,new Identifier(MOD_ID,"mod_artifact_group"),
-            FabricItemGroup.builder().displayName(Text.translatable("artifact-group.bio")).icon(()->new ItemStack(ModBlocks.RELIC_CONTAINER_BLOCK)).entries((displayContext, entries) -> {
+    public static final ItemGroup MOD_ARTIFACT_GROUP = Registry.register(Registries.ITEM_GROUP, new Identifier(MOD_ID, "mod_artifact_group"),
+            FabricItemGroup.builder().displayName(Text.translatable("artifact-group.bio")).icon(() -> new ItemStack(ModItems.VITALITY_PUMP)).entries((displayContext, entries) -> {
                 entries.add(ModItems.ANCIENT_STONE_SWORD);
                 entries.add(ModItems.RADIANT_LOTUS);
                 entries.add(ModItems.SHINY_ANCIENT_STONE_SWORD);
@@ -35,8 +35,11 @@ public class ModItemGroups {
                 entries.add(ModItems.PORTAL_RING);
                 entries.add(ModItems.MIDAS_HAMMER);
                 entries.add(ModItems.GALE_CORE);
+                // disabled until we think this one out
+                // entries.add(ModItems.SILENT_FIRE);
             }).build());
-    public static void registerGroups(){
-        BioTech.LOGGER.info("Registering Item Groups for ---> "+MOD_ID);
+
+    public static void registerGroups() {
+        BioTech.LOGGER.info("Registering Item Groups for ---> " + MOD_ID);
     }
 }
--- src/main/java/com/bajookie/biotech/item/ModItems.java
+++ src/main/java/com/bajookie/biotech/item/ModItems.java
@@ -5,16 +5,17 @@ import com.bajookie.biotech.item.custom.*;
 import com.bajookie.biotech.item.custom.AncientStoneSwordItem;
 import com.bajookie.biotech.item.custom.MidasHammerItem;
 import com.bajookie.biotech.item.custom.PortalRingItem;
-import com.bajookie.biotech.item.custom.PortalRingItem;
 import com.bajookie.biotech.item.custom.VitalityPumpItem;
 import com.bajookie.biotech.item.custom.RadiantLotusItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 
 import static com.bajookie.biotech.BioTech.MOD_ID;
 
@@ -29,15 +30,17 @@ public class ModItems {
     public static final Item VITALITY_PUMP = registerItem("vitality_pump",new VitalityPumpItem(2));
     public static final Item PORTAL_RING = registerItem("portal_ring",new PortalRingItem());
     public static final Item GALE_CORE = registerItem("gale_core",new GaleCoreItem());
+    public static final Item SILENT_FIRE = registerItem("silent_fire", new AuraItem(new FabricItemSettings().maxCount(1).rarity(Rarity.EPIC), StatusEffects.GLOWING, 25, 0));
 
 
 
-    //Register methods
-    public static Item registerItem(String name,Item item){
-        return Registry.register(Registries.ITEM,new Identifier(MOD_ID,name),item);
+    // Register methods
+    public static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(MOD_ID, name), item);
     }
-    public static void registerModItems(){
 
-        BioTech.LOGGER.info("Register Items for:"+MOD_ID);
+    public static void registerModItems() {
+
+        BioTech.LOGGER.info("Register Items for:" + MOD_ID);
     }
 }
