fe77c645b3b4029fb50159dd62204945c1f213a1
liorb <lior.buchmeier@gmail.com>
2023-12-09 14:35:12 +0200

    added raytrace(not working)

--- src/main/java/com/bajookie/biotech/util/RayBox.java
+++ src/main/java/com/bajookie/biotech/util/RayBox.java
@@ -1,13 +1,17 @@
 package com.bajookie.biotech.util;
 
 import com.bajookie.biotech.BioTech;
+import net.minecraft.block.Block;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileUtil;
+import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -37,5 +41,22 @@ public class RayBox {
         } else {
             return null;
         }
+    }
+    @Nullable
+    public static LivingEntity getCollisionLivingEntityRT(long dist,LivingEntity entity,boolean includeFluids){
+        MinecraftClient client = MinecraftClient.getInstance();
+        Vec3d vecStart = entity.getEyePos();
+        Vec3d vecEnd = null;
+        HitResult result = client.cameraEntity.raycast(dist,1f,true);
+        if (result.getType() == HitResult.Type.BLOCK){
+            vecEnd = result.getPos();
+            HitResult res = entity.getWorld().raycast(new RaycastContext(vecStart,vecEnd,RaycastContext.ShapeType.OUTLINE, includeFluids ? RaycastContext.FluidHandling.ANY : RaycastContext.FluidHandling.NONE, entity));
+            BioTech.LOGGER.info(""+res.getType());
+        }else {
+            return null;
+        }
+
+        return null;
+
     }
 }
