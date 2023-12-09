8719c00f35c4736c6f7f8f98a82a4f30e7ebc430
liorb <lior.buchmeier@gmail.com>
2023-12-09 13:09:57 +0200

    fixed the potion mirage potion

--- src/main/java/com/bajookie/biotech/mixin/PotionUseMixin.java
+++ src/main/java/com/bajookie/biotech/mixin/PotionUseMixin.java
@@ -2,6 +2,7 @@ package com.bajookie.biotech.mixin;
 
 import com.bajookie.biotech.BioTech;
 import com.bajookie.biotech.item.ModItems;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.thrown.PotionEntity;
 import net.minecraft.item.*;
@@ -13,16 +14,18 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ThrowablePotionItem.class)
+@Mixin(PotionItem.class)
 public class PotionUseMixin {
-    @Inject(method = "use", at = @At(value = "INVOKE",target = "Lnet/minecraft/item/ItemStack;decrement(I)V",shift = At.Shift.BEFORE), cancellable = true)
-    public void use(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> info) {
-        if (user.getInventory().contains(ModItems.POTION_MIRAGE.getDefaultStack())){
-            int slot = user.getInventory().getSlotWithStack(ModItems.POTION_MIRAGE.getDefaultStack());
-            if (!user.getItemCooldownManager().isCoolingDown(user.getInventory().getStack(slot).getItem())){
-                user.getItemCooldownManager().set(user.getInventory().getStack(slot).getItem(),20*5);
-                info.setReturnValue(TypedActionResult.success(user.getStackInHand(hand)));
-                info.cancel();
+    @Inject(method = "finishUsing", at = @At(value = "INVOKE",target = "Lnet/minecraft/item/ItemStack;decrement(I)V",shift = At.Shift.BEFORE), cancellable = true)
+    public void finishUsing(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> info) {
+        if (user instanceof PlayerEntity player){
+            if (player.getInventory().contains(ModItems.POTION_MIRAGE.getDefaultStack())){
+                int slot = player.getInventory().getSlotWithStack(ModItems.POTION_MIRAGE.getDefaultStack());
+                if (!player.getItemCooldownManager().isCoolingDown(player.getInventory().getStack(slot).getItem())){
+                    player.getItemCooldownManager().set(player.getInventory().getStack(slot).getItem(),20*5);
+                    info.setReturnValue(stack);
+                    info.cancel();
+                }
             }
         }
     }
