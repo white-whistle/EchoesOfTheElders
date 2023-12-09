953eb4484a72a87c0eda62d675e8b07910550d6d
liorb <lior.buchmeier@gmail.com>
2023-12-09 16:37:58 +0200

    added world gen for flowers

--- src/main/java/com/bajookie/biotech/world/ModPlacedFeatures.java
+++ src/main/java/com/bajookie/biotech/world/ModPlacedFeatures.java
@@ -8,8 +8,8 @@ import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.YOffset;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.PlacedFeature;
-import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
-import net.minecraft.world.gen.placementmodifier.PlacementModifier;
+import net.minecraft.world.gen.feature.PlacedFeatures;
+import net.minecraft.world.gen.placementmodifier.*;
 
 import java.util.List;
 
@@ -17,6 +17,8 @@ import static com.bajookie.biotech.BioTech.MOD_ID;
 
 public class ModPlacedFeatures {
     public static final RegistryKey<PlacedFeature> RELIC_CONTAINER_PLACED_KEY = registerKey("relic_container_placed");
+    public static final RegistryKey<PlacedFeature> EXPLORER_FRUIT_PLACED_KEY = registerKey("explorer_fruit");
+    public static final RegistryKey<PlacedFeature> MINERS_SHROOM_PLACED_KEY = registerKey("miners_shroom");
 
     public static void bootstrap(Registerable<PlacedFeature> context){
         var configeredFeatureRegistryEnteryLookup = context.getRegistryLookup(RegistryKeys.CONFIGURED_FEATURE);
@@ -24,6 +26,11 @@ public class ModPlacedFeatures {
                 ModOrePlacement.modifiersWithCount(12, //veins per chunk
                         HeightRangePlacementModifier.uniform(YOffset.fixed(-80),YOffset.fixed(90)//where to place
                         )));
+        register(context,EXPLORER_FRUIT_PLACED_KEY,configeredFeatureRegistryEnteryLookup.getOrThrow(ModConfiguredFeatures.EXPLORERS_FRUIT_KEY),
+                List.of(RarityFilterPlacementModifier.of(4), SquarePlacementModifier.of(), PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of()));
+
+        register(context,MINERS_SHROOM_PLACED_KEY,configeredFeatureRegistryEnteryLookup.getOrThrow(ModConfiguredFeatures.EXPLORERS_FRUIT_KEY),
+                List.of(RarityFilterPlacementModifier.of(4), SquarePlacementModifier.of(), PlacedFeatures.MOTION_BLOCKING_HEIGHTMAP, BiomePlacementModifier.of()));
     }
     public static RegistryKey<PlacedFeature> registerKey(String name) {
         return RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(MOD_ID, name));
