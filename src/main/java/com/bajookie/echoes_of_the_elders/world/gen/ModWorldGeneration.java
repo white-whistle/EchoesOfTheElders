016883510bbf0a010ff39135af0ad6ae7a617874
liorb <lior.buchmeier@gmail.com>
2024-01-01 20:08:34 +0200

    added new biome with ugly color

--- src/main/java/com/bajookie/echoes_of_the_elders/world/gen/ModWorldGeneration.java
+++ src/main/java/com/bajookie/echoes_of_the_elders/world/gen/ModWorldGeneration.java
@@ -1,8 +1,18 @@
 package com.bajookie.echoes_of_the_elders.world.gen;
 
-public class ModWorldGeneration {
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModWorldGeneration{
+
     public static void generateModWorldGen(){
         ModOreGeneration.generateOres();
         ModFlowerGeneration.generateFlowers();
     }
+
+
 }
