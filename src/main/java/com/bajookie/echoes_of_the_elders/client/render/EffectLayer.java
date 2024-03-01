8c81fedcfcc4443193dec0fe861b46428e5c4509
Tomer Atar <kenyweb7@gmail.com>
2024-03-01 15:06:41 +0200

    feat: custom armor layers

--- src/main/java/com/bajookie/echoes_of_the_elders/client/render/EffectLayer.java
+++ src/main/java/com/bajookie/echoes_of_the_elders/client/render/EffectLayer.java
@@ -7,9 +7,6 @@ import net.minecraft.client.render.RenderPhase;
 import net.minecraft.client.render.VertexFormat;
 import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.render.block.entity.EndPortalBlockEntityRenderer;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
 
@@ -18,7 +15,7 @@ import java.util.function.Function;
 @Environment(EnvType.CLIENT)
 public class EffectLayer {
     private static final Function<Identifier, RenderLayer> GLOW = Util.memoize(texture -> {
-        RenderPhase.Texture texture2 = new RenderPhase.Texture((Identifier) texture, false, false);
+        RenderPhase.Texture texture2 = new RenderPhase.Texture(texture, false, false);
         return RenderLayer.of("eote_glow", VertexFormats.POSITION_COLOR_TEXTURE_OVERLAY_LIGHT_NORMAL, VertexFormat.DrawMode.QUADS, 256, false, true, RenderLayer.MultiPhaseParameters.builder().program(RenderPhase.EYES_PROGRAM).texture(texture2).cull(RenderPhase.DISABLE_CULLING).depthTest(RenderPhase.EQUAL_DEPTH_TEST)/*.transparency(GLINT_TRANSPARENCY)*/.transparency(RenderPhase.TRANSLUCENT_TRANSPARENCY).layering(RenderPhase.VIEW_OFFSET_Z_LAYERING).writeMaskState(RenderPhase.COLOR_MASK).build(false));
     });
 
@@ -35,12 +32,5 @@ public class EffectLayer {
 
     public static RenderLayer getPortalMasked(Identifier texture) {
         return PORTAL_MASKED.apply(texture);
-    }
-
-    public static RenderLayer getGlowFromBaseTexture(ItemStack itemStack, EquipmentSlot equipmentSlot, LivingEntity livingEntity, int layer) {
-        var ret = ItemStackCustomArmorTextures.getBaseTexture(itemStack, equipmentSlot, livingEntity);
-        if (ret == null) return null;
-
-        return getGlow(ItemStackCustomArmorTextures.toArmorLayerTexture(ret.withSuffixedPath("_glow"), layer));
     }
 }
