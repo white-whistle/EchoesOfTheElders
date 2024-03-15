c8d7a553e5340be2bcbfc9439ec5e9bad8ed3a0d
LiorBuch <lior.buchmeier@gmail.com>
2024-03-15 16:28:37 +0200

    feat:item can control monolook

--- src/main/java/com/bajookie/echoes_of_the_elders/entity/custom/CompanionEntity.java
+++ src/main/java/com/bajookie/echoes_of_the_elders/entity/custom/CompanionEntity.java
@@ -1,6 +1,9 @@
 package com.bajookie.echoes_of_the_elders.entity.custom;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -8,7 +11,12 @@ import net.minecraft.world.EntityView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public class CompanionEntity extends TameableEntity {
+    protected static final TrackedData<Optional<UUID>> OWNER_UUID = DataTracker.registerData(CompanionEntity.class, TrackedDataHandlerRegistry.OPTIONAL_UUID);
+
     protected CompanionEntity(EntityType<? extends TameableEntity> entityType, World world) {
         super(entityType, world);
     }
--- src/main/java/com/bajookie/echoes_of_the_elders/item/custom/MonolookSpawn.java
+++ src/main/java/com/bajookie/echoes_of_the_elders/item/custom/MonolookSpawn.java
@@ -2,12 +2,17 @@ package com.bajookie.echoes_of_the_elders.item.custom;
 
 import com.bajookie.echoes_of_the_elders.entity.custom.MonolookEntity;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
+
+import java.util.UUID;
 
 public class MonolookSpawn extends Item {
     public MonolookSpawn() {
@@ -16,13 +21,46 @@ public class MonolookSpawn extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack stack = user.getStackInHand(hand);
+        NbtCompound comp = stack.getOrCreateNbt();
         if (!world.isClient){
-            MonolookEntity monolook = new MonolookEntity(world);
-            monolook.setPosition(user.getPos().add(1,3,1));
-            world.spawnEntity(monolook);
-            monolook.setOwner(user);
-            monolook.setOwnerUuid(user.getUuid());
+            ServerWorld serverWorld = (ServerWorld) world;
+            if (!comp.contains("entity_id")){
+                MonolookEntity monolook = new MonolookEntity(world);
+                monolook.setPosition(user.getPos().add(1,3,1));
+                monolook.setOwner(user);
+                monolook.setOwnerUuid(user.getUuid());
+                world.spawnEntity(monolook);
+                comp.putUuid("entity_id",monolook.getUuid());
+            } else {
+                UUID uui = comp.getUuid("entity_id");
+                var e = serverWorld.getEntity(uui);
+                if (e != null){
+                    e.discard();
+                }
+                comp.remove("entity_id");
+            }
         }
         return super.use(world, user, hand);
+    }
+
+    @Override
+    public boolean hasGlint(ItemStack stack) {
+        return stack.getOrCreateNbt().contains("entity_id");
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        super.inventoryTick(stack, world, entity, slot, selected);
+        if (!world.isClient){
+            NbtCompound comp = stack.getOrCreateNbt();
+            if (comp.contains("entity_id")){
+                ServerWorld serverWorld = (ServerWorld) world;
+                var e =serverWorld.getEntity(comp.getUuid("entity_id"));
+                if (e == null){
+                    comp.remove("entity_id");
+                }
+            }
+        }
     }
 }
